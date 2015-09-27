b6073f9a0400b9400e65a0abd566a41c5262e48e
Matt Leonawicz <matt.rstat@gmail.com>
2015-09-27 11:30:05 -0600

    Use data tables, consolidate outputs

--- code/shapes2cells.R
+++ code/shapes2cells.R
@@ -1,11 +1,12 @@
 # @knitr setup
 library(raster)
 library(maptools)
+library(data.table)
+library(dplyr)
 library(parallel)
 
 outDir <- "/workspace/UA/mfleonawicz/projects/DataExtraction/workspaces"
 shpDir <- "/workspace/UA/mfleonawicz/projects/DataExtraction/data/shapefiles"
-set.seed(253)
 
 # Political boundaries
 # Alaska
@@ -27,7 +28,6 @@ eco32_IDs <- gsub("\\.", "", as.data.frame(eco32_shp)[,1])
 eco9_IDs <- sapply(slot(eco9_shp, "polygons"), function(x) slot(x, "ID"))
 eco3_IDs <- sapply(slot(eco3_shp, "polygons"), function(x) slot(x, "ID"))
 
-
 # LCC regions
 LCC_shp <- shapefile(file.path(shpDir, "LCC/LCC_summarization_units_singlepartPolys"))
 LCC_IDs <- gsub(" LCC", "", gsub("South", "S", gsub("western", "W", gsub("Western", "W", gsub("North", "N", gsub("  ", " ", gsub("\\.", "", as.data.frame(LCC_shp)[,1])))))))
@@ -36,54 +36,34 @@ LCC_IDs <- gsub(" LCC", "", gsub("South", "S", gsub("western", "W", gsub("Wester
 CAVM_shp <- shapefile(file.path(shpDir, "CAVM/CAVM_complete"))
 CAVM_IDs <- as.data.frame(CAVM_shp)[,4]
 
-# @knitr organize
-# organize shapefile lists and associated metadata
-shp.names <- c(paste("Political", 0:6), paste0("Alaska L", 3:1, " Ecoregions"), "LCC Regions", "CAVM Regions")
+# shapefile lists, names, and associated metadata
+grp.names <- c(rep("Political Boundaries", 2), paste0("Alaska L", 3:1, " Ecoregions"), "LCC Regions", "CAVM Regions")
 shp.list <- list(Alaska_shp, Canada_shp, eco32_shp, eco9_shp, eco3_shp, LCC_shp, CAVM_shp)
-shp.IDs.list <- list("Alaska", Canada_IDs, eco32_IDs, eco9_IDs, eco3_IDs, LCC_IDs, CAVM_IDs)
-region.names.out <- c(list(c("AK-CAN", unlist(shp.IDs.list[1:2]))), shp.IDs.list[3:length(shp.IDs.list)]) # prefix with full domain
-names(region.names.out) <- c("Political", shp.names[8:length(shp.names)])
+shp.names.list <- list("Alaska", Canada_IDs, eco32_IDs, eco9_IDs, eco3_IDs, LCC_IDs, CAVM_IDs)
 
-# @knitr 1km_AKCAN_alfresco
-# For AK-CAN 1-km ALfresco extractions
-#r <- readAll(raster("/Data/Base_Data/ALFRESCO_formatted/ALFRESCO_Master_Dataset/ALFRESCO_Model_Input_Datasets/AK_CAN_Inputs/Climate/5ModelAvg/sresa2/tas/tas_mean_C_alf_ar4_5modelAvg_sresa2_01_2001.tif"))
+# function to extract cell indices from raster by shapefile and return data table
+get_cells <- function(i, r, shp, grp, loc, idx=Which(!is.na(r),cells=T)){
+    stopifnot(length(shp)==length(grp) & length(shp)==length(loc))
+    x <- extract(r, shp[[i]], cellnumbers=T)
+    stopifnot(length(x)==length(loc[[i]]))
+    for(j in 1:length(x)) if(!is.null(x[[j]])) x[[j]] <- data.table(LocGroup=grp[i], Location=loc[[i]][j], Cell=sort(intersect(x[[j]][,1], idx)))
+    rbindlist(x)
+}
+
+# @knitr 1km_2km_AKCAN
+# For AK-CAN 1-km Alfresco and 2-km climate extractions
 dirs <- list.files("/big_scratch/apbennett/Calibration/FinalCalib", pattern=".*.sres.*.", full=T) # alternate
-r <- readAll(raster(list.files(file.path(dirs[1], "Maps"), pattern="^Age_0_.*.tif$", full=T)[1])) # template
-data.ind <- Which(!is.na(r),cells=T)
-cells_shp_list <- mclapply(1:length(shp.list), function(x, shp, r) extract(r, shp[[x]], cellnumbers=T), shp=shp.list, r=r, mc.cores=32)
-cells_shp_list <- rapply(cells_shp_list, f=function(x, d.ind) intersect(x[,1], d.ind), classes="matrix", how="replace", d.ind=data.ind)
-cells_shp_list <- c(list(c(list(data.ind), cells_shp_list[[1]], cells_shp_list[[2]])), cells_shp_list[-c(1:2)]) # Combine full domain and other political boundaries into one group
+r1km <- readAll(raster(list.files(file.path(dirs[1], "Maps"), pattern="^Age_0_.*.tif$", full=T)[1])) # template
+r2km <- readAll(raster("/Data/Base_Data/Climate/AK_CAN_2km/projected/AR5_CMIP5_models/rcp60/5modelAvg/pr/pr_total_mm_AR5_5modelAvg_rcp60_01_2006.tif")) # template
+idx1 <- Which(!is.na(r1km),cells=T)
+idx2 <- Which(!is.na(r2km),cells=T)
 
-n.shp <- sum(unlist(lapply(cells_shp_list, length)))
-names(cells_shp_list) <- names(region.names.out)
-for(i in 1:length(cells_shp_list)) names(cells_shp_list[[i]]) <- region.names.out[[i]]
+cells1 <- data.table(Source="akcan1km", rbindlist(mclapply(1:length(shp.list), get_cells, r=r1km, shp=shp.list, grp=grp.names, loc=shp.names.list, idx=idx1, mc.cores=32)))
+cells1 <- bind_rows(data.table(Source="akcan1km", LocGroup="Political Boundaries", Location="AK-CAN", Cell=idx1), cells1)
+cells1 <- data.table(cells1) %>% group_by(Location) %>% mutate(Cell_rmNA=which(c(1:ncell(r1km) %in% Cell)[idx1]))
+cells2 <- data.table(Source="akcan2km", rbindlist(mclapply(1:length(shp.list), get_cells, r=r2km, shp=shp.list, grp=grp.names, loc=shp.names.list, idx=idx2, mc.cores=32)))
+cells2 <- bind_rows(data.table(Source="akcan2km", LocGroup="Political Boundaries", Location="AK-CAN", Cell=idx2), cells2)
+cells2 <- data.table(cells2) %>% group_by(Location) %>% mutate(Cell_rmNA=which(c(1:ncell(r2km) %in% Cell)[idx2]))
 
-cells_shp_list_5pct <- rapply(cells_shp_list, f=function(x, pct) sort(sample(x, size=pct*length(x), replace=FALSE)), classes="integer", how="replace", pct=0.05)
-cells_shp_list_rmNA <- rapply(cells_shp_list, f=function(x, n.cells, d.ind) which(c(1:n.cells %in% x)[d.ind]), classes="integer", how="replace", n.cells=ncell(r), d.ind=data.ind)
-cells_shp_list_rmNA_5pct <- rapply(cells_shp_list_5pct, f=function(x, n.cells, d.ind) which(c(1:n.cells %in% x)[d.ind]), classes="integer", how="replace", n.cells=ncell(r), d.ind=data.ind)
-
-save(cells_shp_list, region.names.out, n.shp, file=file.path(outDir, "shapes2cells_AKCAN1km.RData"))
-save(cells_shp_list_5pct, region.names.out, n.shp, file=file.path(outDir, "shapes2cells_AKCAN1km_5pct.RData"))
-save(cells_shp_list_rmNA, region.names.out, n.shp, file=file.path(outDir, "shapes2cells_AKCAN1km_rmNA.RData"))
-save(cells_shp_list_rmNA_5pct, region.names.out, n.shp, file=file.path(outDir, "shapes2cells_AKCAN1km_rmNA_5pct.RData"))
-
-# @knitr 2km_AKCAN_climate
-# For AK-CAN 2-km extractions
-r <- readAll(raster("/Data/Base_Data/Climate/AK_CAN_2km/projected/AR5_CMIP5_models/rcp60/5modelAvg/pr/pr_total_mm_AR5_5modelAvg_rcp60_01_2006.tif")) # template done
-data.ind <- Which(!is.na(r),cells=T)
-cells_shp_list <- mclapply(1:length(shp.list), function(x, shp, r) extract(r, shp[[x]], cellnumbers=T), shp=shp.list, r=r, mc.cores=32)
-cells_shp_list <- rapply(cells_shp_list, f=function(x, d.ind) intersect(x[,1], d.ind), classes="matrix", how="replace", d.ind=data.ind)
-cells_shp_list <- c(list(c(list(data.ind), cells_shp_list[[1]], cells_shp_list[[2]])), cells_shp_list[-c(1:2)]) # Combine full domain and other political boundaries into one group
-
-n.shp <- sum(unlist(lapply(cells_shp_list, length)))
-names(cells_shp_list) <- names(region.names.out)
-for(i in 1:length(cells_shp_list)) names(cells_shp_list[[i]]) <- region.names.out[[i]]
-
-cells_shp_list_5pct <- rapply(cells_shp_list, f=function(x, pct) sort(sample(x, size=pct*length(x), replace=FALSE)), classes="integer", how="replace", pct=0.05)
-cells_shp_list_rmNA <- rapply(cells_shp_list, f=function(x, n.cells, d.ind) which(c(1:n.cells %in% x)[d.ind]), classes="integer", how="replace", n.cells=ncell(r), d.ind=data.ind)
-cells_shp_list_rmNA_5pct <- rapply(cells_shp_list_5pct, f=function(x, n.cells, d.ind) which(c(1:n.cells %in% x)[d.ind]), classes="integer", how="replace", n.cells=ncell(r), d.ind=data.ind)
-
-save(cells_shp_list, region.names.out, n.shp, file=file.path(outDir, "shapes2cells_AKCAN2km.RData"))
-save(cells_shp_list_5pct, region.names.out, n.shp, file=file.path(outDir, "shapes2cells_AKCAN2km_5pct.RData"))
-save(cells_shp_list_rmNA, region.names.out, n.shp, file=file.path(outDir, "shapes2cells_AKCAN2km_rmNA.RData"))
-save(cells_shp_list_rmNA_5pct, region.names.out, n.shp, file=file.path(outDir, "shapes2cells_AKCAN2km_rmNA_5pct.RData"))
+cells <- bind_rows(cells1, cells2) %>% data.table %>% group_by(Source, LocGroup, Location) %>% setkey
+save(cells, file=file.path(outDir, "shapes2cells_akcan1km2km.RData"))
